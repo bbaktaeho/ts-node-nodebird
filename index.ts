cf18b97261736c9fc8dd2190b7cd77978e1ee0db
bbaktaeho <lth9036@gmail.com>
2020-04-04 14:16:34 +0900

    1-3

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
-import * as expres from "express";
-const app = express();
+import * as express from "express";
+import { Request, Response, NextFunction, Application } from "express";
+const app: Application = express();
 
-const prod = process.env.NODE_ENV === "production";
+const prod: boolean = process.env.NODE_ENV === "production";
 
-app.get("/", (req, res) => {
+app.set("port", prod ? process.env.PORT : 3065);
+
+app.get("/", (req: Request, res: Response) => {
   res.send("정상 동작");
 });
 
-app.listen(prod ? process.env.PORT : 3065, () => {
-  console.log(`server is running on ${process.env.PORT}`);
+app.listen(app.get("port"), () => {
+  console.log(`server is running on ${app.get("port")}`);
 });
--- md/one.md
+++ md/one.md
@@ -3,5 +3,8 @@
 cannot read porperty of ...
 타입스크립트를 도입했을 때 안정적
 타입스크립트는 에디터의 도움을 받는게 좋음
-노드가 ts 를 직접 실행 못해서 한 번 컴파일을 해줘야함(ts-node)
+노드가 ts 를 직접 실행 못해서 한 번 컴파일(트랜스파일)을 해줘야함(ts-node)
 import \* as ... from 은 export default 가 없을 때 이렇게 해야함
+esModuleInterop : true 라면 \* as 안해도 됨
+익스프레스는 express-serve-static-core 의 코어를 임포트해와서 타이핑을 함
+타입스크립트를 자바스크립트를 변경 후 노드가 실행을 시킴, ts-node 를 사용해서 바로 실행시키게 해보자
